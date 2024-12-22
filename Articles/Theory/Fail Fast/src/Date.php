905e4cf5c8c6a0bdb806ba0aea6d6e8f7bdfd96a
maxi <maxi@conversalab.com>
2024-12-22 18:01:40 -0300

    Code Smell 284 - Encrypted Functions

--- Articles/Theory/Fail Fast/src/Date.php
+++ Articles/Theory/Fail Fast/src/Date.php
@@ -1,13 +1,15 @@
-<?
+<?php
 
-final class Date{
+final class Date {
 
     function __construct($aMonthDay, $aMonth) {
         if (!$aMonth->includes($aMonthDay)) {
             throw new InvalidDateException($aMonthDay, $aMonth);
-        } 
-        ...
+        }
+        // ...
+    }
 }
+
 $day30 = new Day(30);
 $year2020 = new Year(2020);
 $feb2020 = new YearMonth(2, $year2020);
@@ -15,4 +17,4 @@ $invalidDate = new Date($day30, $feb2020);
 // will raise an exception. 
 // No, It will not coerce to March,1st
 // or do "under the rug magic" 
-// to coverup the programmer contract violation
+// to cover up the programmer contract violation
